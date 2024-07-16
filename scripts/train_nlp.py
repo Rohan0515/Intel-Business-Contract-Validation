43b1be674921d30ba5addeae6494b7bb46c33d38
Rohan J A <137191046+Rohan0515@users.noreply.github.com>
2024-07-16 13:26:53 +0530

    Delete scripts/train_nlp.py

--- scripts/train_nlp.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-def train_nlp_model(training_data_dir):
-    nlp = spacy.load("en_core_web_sm")
-    nlp.to_disk("models/nlp_model/")
-
-if __name__ == "__main__":
-    train_nlp_model("data/preprocessed")
